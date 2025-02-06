961336e9867b1b1c820ded39a040400201e326e3
M O H I T  K U M A R <mohitkr.9933@gmail.com>
2025-02-06 18:28:41 +0530

    ...

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -19,7 +19,7 @@ const Banner = () => {
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#70b28c"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
         </div>
       </div>
-      <div className="display flex items-center justify-center gap-20 mt-8 bg-green-100 text-black px-15 py-5 w-full rounded-lg">
+      <div className="display flex items-center justify-center gap-20 mt-8 text-black px-15 py-5 w-full rounded-lg">
         <FeatCards image={hospital} title="50+ Hospitals"/>
         <FeatCards image={clients} title="2500+ Happy Clients"/>
         <FeatCards image={doctors} title="250+ Doctors"/>
--- src/Components/Items/FeatCards.jsx
+++ src/Components/Items/FeatCards.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const FeatCards = (props) => {
   return (
     <div>
-        <div className='flex flex-col items-center'>
-        <img src={props.image} className="w-1/2 max-w-xs sm:max-w-xs md:max-w-sm lg:max-w-m xl:max-w-lg h-auto" />
+      <div className="flex flex-col items-center bg-green-200 rounded-lg py-4">
+        <img
+          src={props.image}
+          className="w-1/2 max-w-xs sm:max-w-xs md:max-w-sm lg:max-w-m xl:max-w-lg h-auto"
+        />
         <h2>{props.title}</h2>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FeatCards
+export default FeatCards;
