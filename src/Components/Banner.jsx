16dded31ced646064e062715ad567c268da9665d
M O H I T  K U M A R <mohitkr.9933@gmail.com>
2025-02-07 19:54:37 +0530

    ...

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import FeatCards from "./Items/FeatCards";
-import hospital from "../assets/hospital.png";
-import clients from "../assets/clients.png";
-import doctors from "../assets/doctors.png";
-import savings from "../assets/savings.png";
-import location from "../assets/location.png";
+import pros from "../assets/pros.png";
 
 const Banner = () => {
   return (
@@ -19,12 +14,8 @@ const Banner = () => {
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#70b28c"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
         </div>
       </div>
-      <div className="display flex items-center justify-center gap-20 mt-8 text-black px-15 py-5 w-full rounded-lg">
-        <FeatCards image={hospital} title="50+ Hospitals"/>
-        <FeatCards image={clients} title="2500+ Happy Clients"/>
-        <FeatCards image={doctors} title="250+ Doctors"/>
-        <FeatCards image={savings} title="Upto 25% Savings"/>
-        <FeatCards image={location} title="10+ Locations"/>
+      <div className="mt-8">
+        <img src={pros}/>
       </div>
     </div>
   );
--- src/Components/Items/FeatCards.jsx
+++ src/Components/Items/FeatCards.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const FeatCards = (props) => {
   return (
     <div>
-      <div className="flex flex-col items-center bg-green-200 rounded-lg py-4">
+      <div className="flex flex-col items-center justify-between bg-green-200 rounded-lg py-4 min-h-40">
         <img
           src={props.image}
           className="w-1/2 max-w-xs sm:max-w-xs md:max-w-sm lg:max-w-m xl:max-w-lg h-auto"
